7a7c996b04cdadcf0dad35940725ba3ca81e3fa7
pavrda <all@pavrda.cz>
2014-08-05 20:28:35 +0200

    big commit

---
+++ www/data.js
@@ -0,0 +1,213 @@
+var seznam = [
+  		{
+			"title" : "AIG",
+			"icon" : "aig.png",
+			"data" : [ {
+				"title" : "Asistenční služba (cestovní pojišťění)",
+				"number" : "+420 225 341 156"
+			}, {
+				"title" : "Hlášení škod",
+				"number" : "+420 234 108 324"
+			} ]
+		},
+		{
+			"title" : "Allianz pojišťovna",
+			"icon" : "allianz.png",
+			"data" : [ {
+				"title" : "Asisteční služba",
+				"number" : "+420 241 170 000"
+			} ]
+		},
+		{
+			"title" : "AXA ASSISTANCE",
+			"icon" : "axaassistance.png",
+			"data" : [ {
+				"title" : "Asistenční služba",
+				"number" : "+420 272 099 911"
+			} ]
+		},
+		{
+			"title" : "AXA pojišťovna",
+			"icon" : "axa.png",
+			"data" : [ {
+				"title" : "Asistenční služba (pro pojištění vozidel a cestovní pojištění)",
+				"number" : "+420 292 292 292"
+			} ]
+		},
+		{
+			"title" : "Česká podnikatelská pojišťovna",
+			"icon" : "cpp.png",
+			"data" : [ {
+				"title" : "Asistenční služba",
+				"number" : "+420 841 444 555"
+			} ]
+		},
+		{
+			"title" : "Česká pojišťovna",
+			"icon" : "cp.png",
+			"data" : [ {
+				"title" : "Asistenční služba",
+				"number" : "+420 841 114 114"
+			} ]
+		},
+		{
+			"title" : "ČSOB Pojišťovna",
+			"icon" : "csob.png",
+			"data" : [ {
+				"title" : "Asistenční služba",
+				"number" : "+420 222 803 442"
+			}, {
+				"title" : "Hlášení škod",
+				"number" : "+420 800 100 777"
+			} ]
+		},
+		{
+			"title" : "D.A.S. pojišťovna právní ochrany",
+			"icon" : "das.png",
+			"data" : [ {
+				"title" : "Asistenční služba (právní oddělení pro klienty)",
+				"number" : "+420 267 990 777"
+			}, {
+				"title" : "Tísňová linka (naléhavé a neodkladné situace) ",
+				"number" : "+420 603 500 005"
+			} ]
+		},
+		{
+			"title" : "ERGO pojišťovna",
+			"icon" : "ergo.png",
+			"data" : [ {
+				"title" : "Asistenční služba",
+				"number" : "+420 261 222 444"
+			}, {
+				"title" : "Hlášení škod v ČR",
+				"number" : "+420 221 585 205"
+			}, {
+				"title" : "Hlášení škod mimo ČR",
+				"number" : "+420 221 860 626"
+			} ]
+		},
+		{
+			"title" : "ERV Evropská pojišťovna",
+			"icon" : "erv.png",
+			"data" : [ {
+				"title" : "Asistenční služba",
+				"number" : "+420 221 860 606"
+			} ]
+		},
+		{
+			"title" : "Generali pojišťovna",
+			"icon" : "generali.png",
+			"data" : [ {
+				"title" : "Asisteční služba",
+				"number" : "+420 844 188 188"
+			} ]
+		},
+		{
+			"title" : "Hasičská vzájemná pojišťovna",
+			"icon" : "hasicska.png",
+			"data" : [ {
+				"title" : "Asistenční služba POV",
+				"number" : "+420 272 101 062"
+			}, {
+				"title" : "Hlášení škod z povinného ručení",
+				"number" : "+420 272 101 062"
+			}, {
+				"title" : "Hlášení škod ostatní produkty",
+				"number" : "+420 222 119 125"
+			} ]
+		},
+		{
+			"title" : "ING Životní pojišťovna",
+			"icon" : "ing.png",
+			"data" : [ {
+				"title" : "Hlášení pojistných událostí ",
+				"number" : "+420  844 444 480"
+			} ]
+		},
+		{
+			"title" : "Kooperativa pojišťovna",
+			"icon" : "koo.png",
+			"data" : [ {
+				"title" : "Asistenční služba",
+				"number" : "+420 841 105 105"
+			} ]
+		},
+		{
+			"title" : "Maxima pojišťovna",
+			"icon" : "maxima.png",
+			"data" : [
+					{
+						"title" : "Asistenční služba (občanský majetek) ",
+						"number" : "+420 296 339 687"
+					},
+					{
+						"title" : "Asistenční služba (cestovní pojištění a zdravotní pojištění cizinců)",
+						"number" : "+420 296 339 685"
+					}, {
+						"title" : "Hlášení škod",
+						"number" : "+420 273 190 400"
+					} ]
+		}, {
+			"title" : "QBE pojišťovna",
+			"icon" : "qbe.png",
+			"data" : [ {
+				"title" : "Asistenční služba",
+				"number" : "+420 234 094 011"
+			} ]
+		}, {
+			"title" : "Servisní pojišťovna",
+			"icon" : "servisni.png",
+			"data" : [ {
+				"title" : "Asistenční služba",
+				"number" : "+420 800 149 235"
+			}, {
+				"title" : "Hlášení škod",
+				"number" : "+420 800 149 236"
+			} ]
+		}, {
+			"title" : "Slavia pojišťovna",
+			"icon" : "slavia.png",
+			"data" : [ {
+				"title" : "Asistenční služba (cestovní pojištění)",
+				"number" : "+420 296 339 679"
+			}, {
+				"title" : "Asistenční služba (auta)",
+				"number" : "+420 296 000 525"
+			}, {
+				"title" : "Asistenční služba (domácnost a nemovitosti)",
+				"number" : "+420 242 452 652"
+			}, {
+				"title" : "Hlášení škod",
+				"number" : "+420 255 790 111"
+			} ]
+		}, {
+			"title" : "Triglav pojišťovna",
+			"icon" : "triglav.png",
+			"data" : [ {
+				"title" : "Asistenční služba -",
+				"number" : "+420 272 099 910"
+			}, {
+				"title" : "Hlášení škod",
+				"number" : "+420 542 425 000"
+			} ]
+		}, {
+			"title" : "UNIQA pojišťovna",
+			"icon" : "uniqa.png",
+			"data" : [ {
+				"title" : "Asistenční služba",
+				"number" : "+420  800 120 020"
+			} ]
+		}, {
+			"title" : "Wüstenrot pojišťovna",
+			"icon" : "wustenrot.png",
+			"data" : [ {
+				"title" : "Asistenční služba",
+				"number" : "+420 227 231 222"
+			}, {
+				"title" : "Hlášení nových škod",
+				"number" : "+420 800 225 555"
+			}, {
+				"title" : "Informace k nahlášeným škodám",
+				"number" : "+420 257 092 511"
+			}, ]
+		} ];
